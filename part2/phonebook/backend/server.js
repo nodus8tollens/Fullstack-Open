dd194a7f6c3310bf3b27bf35ed22219904305f30
nodus8tollens <mladenovskipredrag@gmail.com>
2024-06-08 17:54:14 +0200

    phonebook backend step 9

---
+++ part2/phonebook/backend/server.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+
+const PORT = 3001;
+
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(cors());
+
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body ")
+);
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.post("/api/persons", (req, res) => {
+  const newId = Math.floor(Math.random() * 1000000);
+  const body = req.body;
+  const personExists = persons.find((p) => p.name === body.name);
+
+  if (personExists) {
+    return res.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: newId,
+  };
+
+  persons = persons.concat(person);
+
+  res.json(person);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((p) => p.id !== id);
+  res.status(204).end();
+});
+
+app.get("/info", (req, res) => {
+  const reqTime = new Date();
+  const formattedTime = reqTime.toString();
+  const numOfPersons = persons.length;
+
+  res.send(`
+    <p>Phonebook has info for ${numOfPersons} people.</p>
+    <br/>
+    <p>${formattedTime}</p>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
