47964caa9ad1ca5d1cd0d2a874579c93b445f85b
nodus8tollens <mladenovskipredrag@gmail.com>
2024-06-08 19:43:32 +0200

    uibuild

--- part2/phonebook/backend/index.js
+++ part2/phonebook/backend/index.js
@@ -10,7 +10,7 @@ morgan.token("body", function (req, res) {
 });
 
 app.use(cors());
-
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body ")
