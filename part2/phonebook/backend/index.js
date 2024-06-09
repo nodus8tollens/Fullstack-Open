512e1ec455a8813a108ee938867b3c104ea2b83a
nodus8tollens <mladenovskipredrag@gmail.com>
2024-06-09 19:38:53 +0200

    uibuild

--- part2/phonebook/backend/index.js
+++ part2/phonebook/backend/index.js
@@ -28,18 +28,13 @@ app.get("/api/persons", (request, response) => {
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
-  if (body.content === undefined) {
-    return response.status(400).json({ error: "content missing" });
+  if (body.number === undefined) {
+    return response.status(400).json({ error: "number missing" });
   }
 
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  });
-
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  if (body.name === undefined) {
+    return response.status(400).json({ error: "name missing" });
+  }
 });
 
 app.get("/api/persons/:id", (request, response) => {
