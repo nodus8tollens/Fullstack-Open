37b5a8fb3c4cc0705dbb40e4d1c8f18e3b2b3fdc
nodus8tollens <mladenovskipredrag@gmail.com>
2024-06-11 16:35:36 +0200

    phonebook database step 4

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -8,6 +8,12 @@ const Person = require("./models/person");
 
 const PORT = process.env.PORT || 3001;
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  next(error);
+};
+
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
 });
@@ -19,13 +25,15 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body ")
 );
 
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then((person) => {
+      response.json(person);
+    })
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   if (request.body.number === undefined) {
     return response.status(400).json({ error: "number missing" });
   }
@@ -39,42 +47,56 @@ app.post("/api/persons", (request, response) => {
     number: request.body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      response.json(person);
+    })
+    .catch((error) => next(error));
 });
 
-app.put("/api/persons/:id", (request, response) => {
+app.put("/api/persons/:id", (request, response, next) => {
   const { name, number } = request.body;
 
   Person.findByIdAndUpdate(request.params.id, { name, number })
     .then((updatedPerson) => response.json(updatedPerson))
-    .catch((error) => console.log("PUT Error: ", error));
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then((person) => {
-    response.status(204).end();
-  });
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((person) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/info", (req, res) => {
+app.get("/info", (request, response, next) => {
   const reqTime = new Date();
   const formattedTime = reqTime.toString();
-  const numOfPersons = persons.length;
 
-  res.send(`
-    <p>Phonebook has info for ${numOfPersons} people.</p>
-    <br/>
-    <p>${formattedTime}</p>
-  `);
+  Person.find({})
+    .then((persons) => {
+      const numOfPersons = persons.length;
+
+      response.send(`
+        <p>Phonebook has info for ${numOfPersons} people.</p>
+        <br/>
+        <p>${formattedTime}</p>
+      `);
+    })
+    .catch((error) => next(error));
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
