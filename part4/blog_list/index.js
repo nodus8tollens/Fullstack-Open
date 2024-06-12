df1ae16099ba766f97303f9a4c1aa256e9376114
nodus8tollens <mladenovskipredrag@gmail.com>
2024-06-12 13:07:52 +0200

    blog list, step 2, config module

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,19 +1,16 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const blogRouter = require("./controllers/blog");
+const config = require("./utils/config");
 
-const mongoUrl = process.env.MONGODB_URI;
-const PORT = process.env.PORT;
-
-mongoose.connect(mongoUrl);
+mongoose.connect(config.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
 });
