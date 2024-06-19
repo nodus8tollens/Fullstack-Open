9520738b2c2021d2e37f95d2d30569ebe5b68063
nodus8tollens <mladenovskipredrag@gmail.com>
2024-06-19 16:01:02 +0200

    Blog List Expansion, step 6

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -4,9 +4,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const blogRouter = require("./controllers/blog");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const config = require("./utils/config");
 const middleware = require("./utils/middleware");
-const logger = require("./utils/logger");
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -23,6 +23,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogRouter);
 app.use("/api/users/", usersRouter);
+app.use("/api/login/", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
