42e416973f63f863223d3f91a0fbbdd4efd0ad33
nodus8tollens <mladenovskipredrag@gmail.com>
2024-07-18 16:48:41 +0200

    Anecdotes, step 6

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,37 +1,10 @@
-import { useSelector, useDispatch } from "react-redux";
-import { voteAnecdote } from "./reducers/anecdoteReducer";
 import AnecdoteForm from "./components/AnecdoteForm";
+import AnecdoteList from "./components/AnecdoteList";
 
 const App = () => {
-  // useSelector is a hook from react-redux that allows us
-  // to extract data from the Redux store state.
-  const anecdotes = useSelector((state) => state);
-  anecdotes.sort((a, b) => b.votes - a.votes);
-
-  // useDispatch is a hook from react-redux that returns
-  // a reference to the dispatch function from the Redux store.
-  const dispatch = useDispatch();
-
-  // Function/React handler for voting on an anecdote
-  const vote = (id) => {
-    console.log("vote", id);
-    // Dispatch the VOTE_ANECDOTE action with
-    // the id of the anecdote to be voted
-    dispatch(voteAnecdote(id));
-  };
-
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   );
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,39 @@
+import { useSelector, useDispatch } from "react-redux";
+import { voteAnecdote } from "../reducers/anecdoteReducer";
+
+const AnecdoteList = () => {
+  // useSelector is a hook from react-redux that allows us
+  // to extract data from the Redux store state.
+  const anecdotes = useSelector((state) => state);
+
+  anecdotes.sort((a, b) => b.votes - a.votes);
+
+  // useDispatch is a hook from react-redux that returns
+  // a reference to the dispatch function from the Redux store.
+  const dispatch = useDispatch();
+
+  // Function/React handler for voting on an anecdote
+  const vote = (id) => {
+    console.log("vote", id);
+    // Dispatch the VOTE_ANECDOTE action with
+    // the id of the anecdote to be voted
+    dispatch(voteAnecdote(id));
+  };
+
+  return (
+    <>
+      <h2>Anecdotes</h2>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default AnecdoteList;
