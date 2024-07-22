915765062d016d03951a7e7afe29474ad89acb2c
nodus8tollens <mladenovskipredrag@gmail.com>
2024-07-22 16:47:07 +0200

    Exercise 6.21

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,11 +1,26 @@
+import { createAnecdote } from "../services/requests";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient();
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+    },
+  });
+
   // Handler for creating new anecdotes
   const addAnecdote = async (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
+    if (content.length < 5) {
+      alert("Content must be at least 5 characters long");
+      return;
+    }
     event.target.anecdote.value = "";
-    // Creates and dispatches a CREATE_ANECDOTE action
-    // with the appropriate payload
+
+    newAnecdoteMutation.mutate({ content, votes: 0 });
   };
 
   return (
