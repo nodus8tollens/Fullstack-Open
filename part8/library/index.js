d22e11c7f81278f00433074b3d6ed54baa1c7694
nodus8tollens <mladenovskipredrag@gmail.com>
2024-09-15 19:17:37 +0200

    8.4: Books of an author

--- part8/library/index.js
+++ part8/library/index.js
@@ -106,10 +106,18 @@ const typeDefs = `
   
   }
 
+  type Author{
+    name: String!,
+    born: Int!,
+    bookCount: Int!
+  
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -117,7 +125,16 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) =>
+      args.author ? books.filter((book) => book.author === args.author) : books,
+    allAuthors: () => {
+      return authors.map((author) => {
+        const bookCount = books.filter(
+          (book) => book.author === author.name
+        ).length;
+        return { ...author, bookCount };
+      });
+    },
   },
 };
 
